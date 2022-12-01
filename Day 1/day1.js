1e6e2b55788560a4c3a7cbc3f2e47fa5353b1ed0
scott-hall7 <scotthall179@gmail.com>
2022-12-01 12:44:12 -0600

    day 1 solution

---
+++ Day 1/day1.js
@@ -0,0 +1,18 @@
+// Gets input from html text content
+const input = document.body.textContent;
+
+
+//  Splits sections into into an array of arrays
+const splitArrays = input.trim().split(/\r?\n\r?\n/).map(elf => elf.split(/\r?\n/));
+
+
+//  Finds sum in each elf array and creates new array of all the sums
+const totalCalories = splitArrays.map(array => array.reduce((accumulator, currentValue) => accumulator + (currentValue * 1), 0));
+
+//  Sorts the sum totals by smallest to largest
+const sortedCalories = totalCalories.sort((a,b) => a - b)
+
+let part1 = sortedCalories.at(-1)
+let part2 = sortedCalories.slice(-3).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+
